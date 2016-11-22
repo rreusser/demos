26b038da720658d26cfd6ae2a36d03de018ad51f
Ricky Reusser <rsreusser@gmail.com>
2016-11-22 18:26:17 -0500

    Multiple contour sets!

--- carpet/index.js
+++ carpet/index.js
@@ -16,14 +16,17 @@ const ellipseCircumf = (a, b) => {
   return Math.PI * (a + b) * (1 + 3 * h / (10 + Math.sqrt(4 - 3 * h)));
 }
 
+const secondFunc = (a, b) => Math.cos(a) * Math.sin(b);
+
 // a/b sampling:
 const abrange = [[1, 7], [1, 5]];
 const abdims = [71, 51];
-const abstride = [5, 5]
+const abstride = [10, 10]
 
 // Define the contours:
-const zrange = [0, 40];
-const zlevels = 21;
+const zrange = [[0, 40], [-1, 1]];
+const zcolors = [[1, 0, 0, 1], [0, 1, 0, 1]];
+const zlevels = [21, 7];
 
 // Basis for a and b:
 const a = linspace(abrange[0][0], abrange[0][1], abdims[0], {dtype: 'float32'});
@@ -35,7 +38,10 @@ const x = xy.pick(null, null, 0);
 const y = xy.pick(null, null, 1);
 const ab = vecFill(pool.zeros([abdims[0], abdims[1], 2], 'float32'), (i, j) => [a.get(i), b.get(j)]);
 const z = fill(pool.zeros([abdims[0], abdims[1]], 'float32'), (i, j) => surfaceArea(a.get(i), b.get(j)));
-const data = fill(pool.zeros([abdims[0], abdims[1]], 'float32'), (i, j) => ellipseCircumf(a.get(i), b.get(j)));
+const data = [
+  fill(pool.zeros([abdims[0], abdims[1]], 'float32'), (i, j) => ellipseCircumf(a.get(i), b.get(j))),
+  fill(pool.zeros([abdims[0], abdims[1]], 'float32'), (i, j) => secondFunc(a.get(i), b.get(j)))
+];
 
 ops.assign(y, z);
 fill(x, (i, j) => i - j);
@@ -48,26 +54,28 @@ const curves = [{
   vertices: xy.data,
   vertexStride: 4,
   elements: gridConnectivity(x, {stride: abstride}),
-  color: [0, 0, 0, 0.4]
+  color: [0, 0, 0, 0.3]
 }];
 
-linspace(zrange[0], zrange[1], zlevels).data.map((level) => {
-  let curve = extractContour(cellsFromGrid(abdims), data.data, level);
-  if (!curve.cells.length) return;
-  curves.push({
-    vertices: regl.buffer(curve.vertexWeights.map((w, i) => {
-      let i1 = curve.vertexIds[i][0] * 2;
-      let i2 = curve.vertexIds[i][1] * 2;
-      return [
-        w * xy.data[i1] + (1 - w) * xy.data[i2],
-        w * xy.data[i1 + 1] + (1 - w) * xy.data[i2 + 1]
-      ]
-    })),
-    vertexStride: 8,
-    elements: curve.cells,
-    color: [1, 0, 0, 1]
-  });
-})
+data.map(function (datum, i) {
+  linspace(zrange[i][0], zrange[i][1], zlevels[i]).data.map((level) => {
+    let curve = extractContour(cellsFromGrid(abdims), datum.data, level);
+    if (!curve.cells.length) return;
+    curves.push({
+      vertices: regl.buffer(curve.vertexWeights.map((w, j) => {
+        let i1 = curve.vertexIds[j][0] * 2;
+        let i2 = curve.vertexIds[j][1] * 2;
+        return [
+          w * xy.data[i1] + (1 - w) * xy.data[i2],
+          w * xy.data[i1 + 1] + (1 - w) * xy.data[i2 + 1]
+        ]
+      })),
+      vertexStride: 8,
+      elements: curve.cells,
+      color: zcolors[i]
+    });
+  })
+});
 
 const drawCurves = regl({
   frag: `
