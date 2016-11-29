9b65eef0483869238be9476870764690f3835249
Ricky Reusser <rsreusser@gmail.com>
2016-11-29 09:40:46 -0500

    Raycast!

--- waves/index.js
+++ waves/index.js
@@ -1,5 +1,7 @@
 const regl = require('regl')({extensions: ['OES_texture_float'], pixelRatio: 1});
 const intersect = require('ray-plane-intersection');
+const length = require('gl-vec3/length');
+const subtract = require('gl-vec3/subtract');
 const gpu = require('./utils')(regl);
 const shape = [150, 150];
 const size = [40, 40];
@@ -23,10 +25,10 @@ const integrate = gpu.op({
     uniform sampler2D y;
     varying vec2 uv;
     void main () {
-      float c = 0.1;   // Wave speed
-      float b = 0.02;  // Damping constant
+      float c = 0.5;   // Wave speed
+      float b = 0.1;  // Damping constant
       float dt = 0.05; // time step
-      float k = 0.01;  // spring constant (pulls things back to zero)
+      float k = 0.001;  // spring constant (pulls things back to zero)
       vec4 f = texture2D(y, uv);
       vec2 e = texture2D(y, uv + du).zw;
       vec2 w = texture2D(y, uv - du).zw;
@@ -95,8 +97,9 @@ const draw = regl({
   frag: `
     precision mediump float;
     uniform vec4 color;
+    uniform float opacity;
     void main() {
-      gl_FragColor = vec4(0.3, 0.5, 1.0, 0.5);
+      gl_FragColor = vec4(0.3, 0.5, 1.0, opacity);
     }
   `,
   vert: `
@@ -116,7 +119,10 @@ const draw = regl({
     func: {srcRGB: 'src alpha', srcAlpha: 1, dstRGB: 1, dstAlpha: 1},
     equation: {rgb: 'add', alpha: 'add'}
   },
-  uniforms: {points: regl.prop('points')},
+  uniforms: {
+    points: regl.prop('points'),
+    opacity: regl.prop('opacity')
+  },
   depth: {enable: false},
   primitive: 'points',
   count: shape[0] * shape[1]
@@ -141,6 +147,7 @@ const camera = require('regl-camera')(regl, {
 
 var raycastMouse = require('./raycast-mouse')(regl);
 
+var phit;
 regl.frame(({tick}) => {
   regl.clear({color: [0, 0, 0, 1]});
 
@@ -148,12 +155,20 @@ regl.frame(({tick}) => {
     raycastMouse(context, ({rayOrigin, rayDirection, rayChanged}) => {
       if (rayChanged) {
         var hit = intersect([], rayOrigin, rayDirection, [0, 1, 0], 0);
-        doBump(0.3, hit[0], hit[2]);
+        if (hit && hit.length && phit && phit.length) {
+          var diff = subtract([], hit, phit);
+          doBump(Math.min(4, length(diff)), hit[0], hit[2]);
+        }
+        phit = hit;
       }
 
       iterate(20);
 
-      draw({points: y1})
+      var eye = context.eye;
+      var phi = Math.atan2(eye[1], Math.sqrt(eye[0] * eye[0] + eye[2] * eye[2]))
+      var diag = Math.sqrt(context.viewportWidth * context.viewportWidth + context.viewportHeight * context.viewportHeight) / 500;
+      var opac = Math.min(1, Math.max(0.05, 20 * Math.atan(1 / length(context.eye)) * diag * Math.sin(phi * 2)));
+      draw({points: y1, opacity: opac})
     });
   });
 });
