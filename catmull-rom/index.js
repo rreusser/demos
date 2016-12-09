95492f5ec73fe6dff6c81d66be022a7e46e78fb0
Ricky Reusser <rsreusser@gmail.com>
2016-12-09 15:17:57 -0500

    Buildlddd

---
+++ catmull-rom/index.js
@@ -0,0 +1,171 @@
+var regl = require('regl')();
+var length = require('gl-vec2/length');
+var sub = require('gl-vec2/subtract');
+
+// generalized Catmull-Rom splines, per
+// http://www.cemyuksel.com/research/catmullrom_param/catmullrom.pdf
+var CatmullRomExp = 0.5;
+function makeContinuousTangent(p0, p1, p2, smoothness) {
+    var d1x = p0[0] - p1[0],
+        d1y = p0[1] - p1[1],
+        d2x = p2[0] - p1[0],
+        d2y = p2[1] - p1[1],
+        d1a = Math.pow(d1x * d1x + d1y * d1y, CatmullRomExp / 2),
+        d2a = Math.pow(d2x * d2x + d2y * d2y, CatmullRomExp / 2),
+        numx = (d2a * d2a * d1x - d1a * d1a * d2x) * smoothness,
+        numy = (d2a * d2a * d1y - d1a * d1a * d2y) * smoothness,
+        denom1 = 3 * d2a * (d1a + d2a),
+        denom2 = 3 * d1a * (d1a + d2a);
+      var dxL = p1[0] + (denom1 && numx / denom1);
+      var dyL = p1[1] + (denom1 && numy / denom1);
+      var dxU = p1[0] - (denom2 && numx / denom2);
+      var dyU = p1[1] - (denom2 && numy / denom2);
+
+    return [[dxL, dyL], [dxU, dyU]];
+}
+
+window.createSplineEvaluator = function splineEvaluator (pts, smoothness) {
+  if (pts.length < 3) {
+    return function (t) {
+      var x = (1 - t) * pts[0][0] + t * pts[1][0];
+      var y = (1 - t) * pts[0][1] + t * pts[1][1];
+      return [x, y];
+    };
+  } else {
+    // Compute the tangents *once* at the beginning, and store them:
+    var tangents = [];
+    for(i = 1; i < pts.length - 1; i++) {
+        tangents.push(makeContinuousTangent(pts[i - 1], pts[i], pts[i + 1], smoothness));
+    }
+
+    return function (param) {
+      var a, b, c, d, p0, p1, p2, p3, i, t, ot;
+      param = Math.max(0, Math.min(pts.length - 1, param));
+      i = Math.min(Math.floor(param), pts.length - 2);
+      t = param - i;
+      ot = 1 - t;
+
+      p0 = pts[i];
+      p3 = pts[i + 1];
+
+      if (i === 0 || i === pts.length - 2) {
+        // Evaluate the quadratic first and last segments;
+        p1 = i === 0 ? tangents[i][0] : tangents[i - 1][1];
+        a = ot * ot;
+        b = 2 * ot * t;
+        c = t * t;
+        return [
+          a * p0[0] + b * p1[0] + c * p3[0],
+          a * p0[1] + b * p1[1] + c * p3[1]
+        ];
+      } else {
+        // Evaluate internal cubic spline segments:
+        p1 = tangents[i - 1][1];
+        p2 = tangents[i][0];
+        p3 = pts[i + 1];
+
+        a = ot * ot * ot;
+        b = 3 * ot * ot * t;
+        c = 3 * ot * t * t;
+        d = t * t * t;
+
+        return [
+          a * p0[0] + b * p1[0] + c * p2[0] + d * p3[0],
+          a * p0[1] + b * p1[1] + c * p2[1] + d * p3[1]
+        ];
+      }
+    }
+  }
+}
+
+function nearestPoint (pts, xy) {
+  var lmin = Infinity;
+  var hit;
+  for (var i = 0; i < pts.length; i++) {
+    var dist = length(sub([], xy, pts[i]));
+    if (dist < lmin) {
+      lmin = dist;
+      hit = pts[i];
+    }
+  }
+  return hit;
+}
+
+var pbuttons, nearest;
+require('mouse-change')(function (buttons, i, j) {
+  if (buttons && 0x01) {
+    var xy = [
+      2 * i / window.innerWidth - 1,
+      2 * (1 - j / window.innerHeight) - 1
+    ];
+
+    if (!pbuttons) {
+      nearest = nearestPoint(controlPoints, xy);
+    }
+
+    nearest[0] = xy[0];
+    nearest[1] = xy[1];
+    update();
+  }
+
+  pbuttons = buttons;
+});
+
+var controlPoints = [[-0.7, 0], [-0.6, 0], [-0.5, 0], [0, 0.1], [0.7, 0]];
+var nctrl = controlPoints.length;
+var controlPointBuf;
+var evaluator = createSplineEvaluator(controlPoints, 1);
+
+var neval = 100;
+var evalPoints = [];
+var evalPointBuf;
+
+function update () {
+  controlPointBuf = (controlPointBuf ? controlPointBuf : regl.buffer)(controlPoints);
+  evaluator = createSplineEvaluator(controlPoints, 1);
+
+  for (var i = 0; i < neval; i++) {
+    evalPoints[i] = evaluator(i / (neval - 1) * controlPoints.length);
+  }
+  evalPointBuf = (evalPointBuf ? evalPointBuf : regl.buffer)(evalPoints);
+}
+
+update();
+
+var drawPoints = regl({
+  frag: `
+    precision mediump float;
+    uniform vec3 color;
+    void main () {
+      if(length(gl_PointCoord.xy - 0.5) > 0.5) discard;
+      gl_FragColor = vec4(color, 1);
+    }
+  `,
+  vert: `
+    precision mediump float;
+    attribute vec2 xy;
+    uniform float rad;
+    void main () {
+      gl_Position = vec4(xy, 0, 1);
+      gl_PointSize = rad;
+    }
+  `,
+  attributes: {
+    xy: {
+      buffer: regl.prop('xy'),
+      stride: 8
+    }
+  },
+  count: regl.prop('n'),
+  uniforms: {
+    rad: regl.prop('rad'),
+    color: regl.prop('color')
+  },
+  primitive: 'points',
+  depth: {enable: false},
+});
+
+regl.frame(({tick}) => {
+  drawPoints({xy: evalPointBuf, n: neval, rad: 5, color: [0, 0, 0]});
+  drawPoints({xy: controlPointBuf, n: nctrl, rad: 10, color: [1, 0, 0]});
+});
--- plotly-unsupported/src/carpet.js
+++ plotly-unsupported/src/carpet.js
@@ -12,20 +12,22 @@ document.body.appendChild(gd);
 
 
 const ellipsoidSA = (a, b) => {
-  return Math.sqrt(4 * Math.PI * Math.pow((
+  return Math.pow(4 * Math.PI * Math.pow((
     Math.pow(a * b, 1.6) +
     Math.pow(a, 1.6) +
     Math.pow(b, 1.6)
-  ) / 3, 1 / 1.6));
+  ) / 3, 1 / 1.6), 0.1);
 }
 
-const na = 41;
-const nb = 31;
-const abrange = [[0.5, 7], [0.5, 5]];
+const na = 4;
+const nb = 3;
+const abrange = [[0.1, 7], [0.1, 5]];
 const arange = [0.5, 7];
 const brange = [0.5, 5];
-const a = linspace(ndarray([], [na]), arange[0], arange[1]);
-const b = linspace(ndarray([], [nb]), brange[0], brange[1]);
+//const a = linspace(ndarray([], [na]), arange[0], arange[1]);
+//const b = linspace(ndarray([], [nb]), brange[0], brange[1]);
+const a = ndarray([0.1, 0.5, 2, 4]);
+const b = ndarray([0.5, 4.5, 5.0]);
 const y = fill(ndarray([], [na, nb]), (i, j) => ellipsoidSA(a.get(i), b.get(j)));
 
 Plotly.plot(gd, [{
@@ -36,6 +38,11 @@ Plotly.plot(gd, [{
   cheaterslope: 1.0,
   type: 'carpet',
   aaxis: {
+    tickmode: 'linear',
+    tick0: 0.2,
+    dtick: 0.2,
+    smoothing: 1.0,
+    cheatertype: 'index',
     showlabels: 'both',
     showlabelprefix: 'first',
     labelpadding: 10,
@@ -46,7 +53,7 @@ Plotly.plot(gd, [{
       family: 'sans-serif'
     },
     gridoffset: 0,
-    gridstep: 10,
+    gridstep: 1,
     gridwidth: 1,
     gridcolor: '#444',
     minorgridoffset: 0,
@@ -55,6 +62,11 @@ Plotly.plot(gd, [{
     minorgridcolor: '#ccc'
   },
   baxis: {
+    tickmode: 'linear',
+    tick0: 0.2,
+    dtick: 0.2,
+    smoothing: 1,
+    cheatertype: 'index',
     showlabels: 'end',
     showlabelprefix: 'all',
     labelpadding: 10,
@@ -64,12 +76,12 @@ Plotly.plot(gd, [{
       size: 12,
       family: 'sans-serif'
     },
-    gridoffset: 5,
-    gridstep: 10,
+    gridoffset: 0,
+    gridstep: 1,
     gridwidth: 1,
     gridcolor: '#444',
-    minorgridoffset: 1,
-    minorgridstep: 2,
+    minorgridoffset: 0,
+    minorgridstep: 1,
     minorgridwidth: 1,
     minorgridcolor: '#ccc',
     showstartlabel: true,
