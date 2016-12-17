970c19760936c09040f7c938b5dca22a4c80bbcf
Ricky Reusser <rsreusser@gmail.com>
2016-12-17 08:59:36 -0500

    Fake some viscosity

--- gravity/index.js
+++ gravity/index.js
@@ -5,21 +5,23 @@ css(fs.readFileSync(__dirname + '/index.css', 'utf8'));
 
 const regl = require('regl')({
   extensions: ['OES_texture_float', 'OES_texture_float_linear'],
-  pixelRatio: 1,
+  pixelRatio: 1.0,
   onDone: (err) => err && require('fail-nicely')(err)
 });
 
 const gpu = require('../regl-cwise')(regl);
 const randn = require('random-normal');
 const swap = require('./swap');
-const boundaryOpts = {xboundary: 'repeat', yboundary: 'repeat', magfilter: 'linear', minfilter: 'linear'};
+const fboOpts = {xboundary: 'repeat', yboundary: 'repeat', magfilter: 'linear', minfilter: 'linear'};
 
-const ptexsize = 1024;
-const ctexsize = 512;
+const ptexsize = 512;
+const ctexsize = 256;
 const stexsize = 512;
-const decay = 1.5;
-const multiplier = 1;
-const dt = 0.016;
+const decayTime = 1000;
+const viscosityTime = 32;
+const multiplier = 0.014;
+const dt = 0.002;
+const G = 1.0;
 
 
 
@@ -27,44 +29,58 @@ const dt = 0.016;
 
 // Particle fbos:
 var dist;
-if (false) {
+if (true) {
+  dist = () => [
+    Math.random(),
+    Math.random(),
+    randn() * 0.1,
+    randn() * 0.1
+  ];
+} else if (false) {
   dist = () => {
-    let x = 0.5 + 0.1 * randn();
-    let y = 0.5 + 0.1 * randn();
+    if (Math.random() < 0.1) {
+      return [0.5, 0.5, 0, 0];
+    }
+
+    var theta = Math.random() * Math.PI * 2;
+    var radius = Math.random() * Math.random() * 0.2
+    let x = 0.5 + Math.cos(theta) * radius;
+    let y = 0.5 + Math.sin(theta) * radius;
 
     let r = Math.sqrt(Math.pow(y - 0.5, 2) + Math.pow(x - 0.5, 2));
-    let v = Math.pow(r, -0.4) * 0.2;
+    let v = Math.pow(r, -0.4) * 5.0;
     let vx = -(y - 0.5) * v;
     let vy = (x - 0.5) * v;
 
     return [x, y, vx, vy];
   }
-} else if (true) {
-  dist = () => [Math.random(), Math.random(), randn() * 0.005, randn() * 0.005];
+} else if (false) {
+  dist = () => {
+    var x = (randn() - 0.5) / 8
+    var y = (randn() - 0.5) / 8
+    return [
+      x, y,
+      randn() * 0.01 + y * 3,
+      randn() * 0.01 - x * 3
+    ];
+  }
 } else {
   dist = (i, j) => {
-    do {
-      var x = Math.random();
-      var y = Math.random();
-      var r = Math.sqrt((x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5));
-    } while (Math.random() > Math.exp(-r * 50))
-
-    var vx = (y - 0.5);
-    var vy = -(x - 0.5);
-    var v = Math.sqrt(vx * vx + vy * vy);
-
-    vx /= v;
-    vy /= v;
-
-    v = Math.pow(r, 1.0) * 1.5;
-
-    vx *= v;
-    vy *= v;
+    var rad = 0.15;
+    var vel = 0.15;
+    if (Math.random() > 0.5) {
+      x = 0.25 + randn() * rad
+      y = 0.5 + randn() * rad
+      vx = 0.5 + randn() * vel;
+      vy = 0.1 + randn() * vel;
+    } else {
+      x = -0.25 + randn() * rad
+      y = 0.5 + randn() * rad
+      vx = -0.5 + randn() * vel;
+      vy = -0.1 + randn() * vel;
+    }
 
     return [x, y, vx ,vy];
-      //randn() * 0.001 + 1.1 * (y - 0.5),
-      //randn() * 0.001 - 1.1 * (x - 0.5)
-    //];
   };
 }
 const pshape = [ptexsize, ptexsize, 4];
@@ -78,15 +94,103 @@ const ycoords = y[0].samplerCoords();
 // Continuum fbos:
 const cshape = [ctexsize, ctexsize, 4];
 const h = 1.0 / cshape[0];
-const rho = gpu.array(() => [0, 0, 0, 0], cshape, boundaryOpts);
+const rho = gpu.array(() => [0, 0, 0, 0], cshape);
 const phi = [
-  gpu.array(() => [0, 0, 0, 0], cshape, boundaryOpts),
-  gpu.array(() => [0, 0, 0, 0], cshape, boundaryOpts)
+  gpu.array(() => [0, 0, 0, 0], cshape, fboOpts),
+  gpu.array(() => [0, 0, 0, 0], cshape, fboOpts)
 ];
-
+const velAccum = [
+  gpu.array(() => [0, 0, 0, 0], cshape, fboOpts),
+  gpu.array(() => [0, 0, 0, 0], cshape, fboOpts)
+];
 const loopbuf = gpu.array(null, [stexsize, stexsize, 4]);
 
-const splatDensity = regl({
+const splatParticles = regl({
+  frag: `
+    precision mediump float;
+    varying vec2 uv;
+    void main () {
+      gl_FragColor = vec4(1, uv, 0);
+    }`,
+  vert: `
+    attribute vec2 xy;
+    uniform sampler2D src;
+    varying vec2 uv;
+    void main () {
+      vec4 val = texture2D(src, xy);
+      uv = val.zw;
+      gl_Position = vec4(val.xy * 2.0 - 1.0, 0, 1);
+      gl_PointSize = 1.0;
+    }
+  `,
+  attributes: {xy: ycoords},
+  uniforms: {src: regl.prop('src')},
+  framebuffer: regl.prop('dest'),
+  count: n,
+  primitive: 'points',
+  blend: {
+    enable: true,
+    func: {srcRGB: 1, srcAlpha: 1, dstRGB: 1, dstAlpha: 1}
+  },
+  depth: {enable: false}
+});
+
+const renderToTexture = regl({
+  frag: `
+    precision mediump float;
+    uniform float alpha;
+    varying vec4 state;
+    uniform float viscosity;
+    varying vec4 dens;
+    void main () {
+      vec2 vavg = dens.yz / max(dens.x, 1.0);
+      float heating = length(state.zw - vavg) * exp(-dens.x * viscosity);
+      gl_FragColor = vec4(
+        0.2 * (1.0 - heating) + 0.8 * heating,
+        0.1,
+        1.0 * (1.0 - heating),
+        1.0
+      ) * alpha;
+    }`,
+  vert: `
+    attribute vec2 xy;
+    uniform sampler2D src;
+    uniform sampler2D rho;
+    varying vec4 dens;
+    varying vec4 state;
+    void main () {
+      state = texture2D(src, xy);
+      dens = texture2D(rho, state.xy);
+      state.xy = state.xy * 2.0 - 1.0;
+      gl_Position = vec4(state.xy, 0, 1);
+      gl_PointSize = 1.0;
+    }
+  `,
+  attributes: {xy: ycoords},
+  uniforms: {
+    src: regl.prop('src'),
+    rho: regl.prop('rho'),
+    alpha: (context, props) => {
+      return 0.05 / Math.sqrt(n) * Math.sqrt(cshape[0] * cshape[0]) * (props.multiplier || 1)
+    },
+    viscosity: (context, props) => dt / viscosityTime,
+  },
+  framebuffer: regl.prop('dest'),
+  count: n,
+  primitive: 'points',
+  blend: {
+    enable: true,
+    func: {
+      srcRGB: 1,
+      srcAlpha: 1,
+      dstRGB: 1,
+      dstAlpha: 1
+    }
+  },
+  depth: {enable: false}
+});
+
+const accumulateVelocity = regl({
   frag: `
     precision mediump float;
     uniform float alpha;
@@ -133,7 +237,7 @@ const relaxPoisson = gpu.map({
   ],
   permute: [1, 0, 2, 3],
   body: `
-    #define G 0.5
+    #define G ${G.toFixed(5)}
     #define PI 3.14159265358979
     vec4 compute (vec4 phi, vec4 rho, float h2, vec4 phin, vec4 phis, vec4 phie, vec4 phiw) {
       return vec4(0.25 * (phin.x + phis.x + phie.x + phiw.x) - PI * G * rho.x * h2, 0.0, 0.0, 0.0);
@@ -147,21 +251,22 @@ const gravitate = regl({
     varying vec2 uv;
     uniform sampler2D src;
     uniform sampler2D phi;
-    //uniform sampler2D rho;
+    uniform sampler2D rho;
     uniform float h;
     uniform float dt;
-    uniform float decay;
+    uniform float globalDecay;
+    uniform float viscosity;
     void main () {
       vec4 y = texture2D(src, uv);
       float dphidx = (texture2D(phi, y.xy + vec2(h, 0)).x - texture2D(phi, y.xy - vec2(h, 0)).x) * 0.5 / h;
       float dphidy = (texture2D(phi, y.xy + vec2(0, h)).x - texture2D(phi, y.xy - vec2(0, h)).x) * 0.5 / h;
-      //float dens = texture2D(rho, y.xy).x;
-      //dphidx = 1.0;
-      //dphidy = 0.0;
-      y = y + vec4(y.z, y.w, -dphidx, -dphidy) * dt;
+      vec3 rho = texture2D(rho, y.xy).xyz;
+      vec2 vavg = rho.yz / max(rho.x, 1.0) * 2.0;
+      y += vec4(y.z, y.w, -dphidx, -dphidy) * dt;
       y.xy = mod(y.xy, vec2(1, 1));
-      //y.zw *= exp(-dens / 50.0);
-      y.zw *= decay;
+      float viscousDecay = exp(-rho.x * viscosity);
+      y.zw = y.zw * viscousDecay + (vavg - y.zw) * (1.0 - viscousDecay);
+      y.zw *= globalDecay;
       gl_FragColor = y;
     }`,
   vert: `
@@ -179,7 +284,8 @@ const gravitate = regl({
     dt: regl.prop('dt'),
     rho: regl.prop('rho'),
     h: h,
-    decay: regl.prop('decay')
+    viscosity: (context, props) => dt / viscosityTime,
+    globalDecay: (context, props) => Math.exp(-dt / props.decayTime)
   },
   framebuffer: regl.prop('dest'),
   depth: {enable: false},
@@ -194,9 +300,8 @@ const drawToScreen = regl({
     uniform float xmul, ymul;
     void main () {
       vec2 uvloop = mod(uv / vec2(xmul, ymul), 1.0);
-      float col = texture2D(src, uvloop).x;
-      col = sqrt(col);
-      gl_FragColor = vec4(vec3(col), 1);
+      vec3 col = texture2D(src, uvloop).xyz;
+      gl_FragColor = vec4(col, 1);
     }
   `,
   vert: `
@@ -285,39 +390,40 @@ function computeMean (fbo) {
 const relaxArgs = [phi[0], phi[1], rho, h * h];
 const debiasArgs = [];
 
-function iterate (tick) {
-  var t1 = Date.now();
-  rho.use(() => regl.clear({color: [0, 0, 0, 1]}));
-  splatDensity({dest: rho, src: y[0]});
+regl.frame(({tick}) => {
+  regl.clear({color: [0, 0, 0, 1]});
 
-  var t2 = Date.now();
   relaxPoisson(relaxArgs, tick === 1 ? 200 : 50);
 
-  var t3 = Date.now();
-  debias({dest: relaxArgs[1], src: relaxArgs[0]});
+  debias({
+    dest: relaxArgs[1],
+    src: relaxArgs[0]
+  });
 
   swap(relaxArgs, 1, 0);
 
-  var t4 = Date.now();
-  var d = Math.exp(-dt / (decay / (0.2 + 0.8 * Math.exp(-tick / 400))));
-  gravitate({dest: y[1], src: y[0], rho: rho, phi: relaxArgs[0], dt: dt, decay: d});
+  gravitate({
+    dest: y[1],
+    src: y[0],
+    rho: rho,
+    phi: relaxArgs[0],
+    dt: dt,
+    decayTime: decayTime
+  });
 
-  var t5 = Date.now();
   swap(y, 0, 1);
-
-  return [t2 - t1, t3 - t2, t4 - t3, t5 - t4];
-}
-
-regl.frame(({tick}) => {
-  var t1 = Date.now();
-  regl.clear({color: [0, 0, 0, 1]});
-
-  iterate(tick);
 
   loopbuf.use(() => regl.clear({color: [0, 0, 0, 1]}));
 
-  splatDensity({dest: loopbuf, src: y[1], multiplier: multiplier})
+  rho.use(() => regl.clear({color: [0, 0, 0, 1]}));
+  splatParticles({dest: rho, src: y[1]});
+
+  renderToTexture({
+    dest: loopbuf,
+    rho: rho,
+    src: y[1],
+    multiplier: multiplier * Math.pow(stexsize / 16, 2)
+  });
 
   drawToScreen({src: loopbuf});
-  var t2 = Date.now();
 });
