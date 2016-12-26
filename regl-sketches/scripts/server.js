52dfff9037c71bb7f08b232f947a83b84164ce51
Ricky Reusser <rsreusser@gmail.com>
2016-12-26 10:20:50 -0500

    Add issue

--- regl-sketches/scripts/server.js
+++ regl-sketches/scripts/server.js
@@ -8,5 +8,5 @@ if (args[0]) {
   args[0] = /\.js/.test(args[0]) ? args[0] : args[0] + '.js';
 }
 
-args = args.concat(['--host', 'localhost', '--force-default-index', '--live', '--open', '--', '-t', 'brfs', '-t', 'glslify', '-t', 'es2040']);
+args = args.concat(['--host', 'localhost', '--force-default-index', '--live', '--open', '--', '-t', 'brfs', '-t', 'es2040', '-g', 'glslify']);
 require('../node_modules/budo').cli(args)
