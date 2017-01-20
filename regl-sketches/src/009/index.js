efa77de38c34fa3123bc5a26fb76a233f0872b0f
Ricky Reusser <rsreusser@gmail.com>
2017-01-20 18:10:54 -0500

    Wheee

--- regl-sketches/src/009/index.js
+++ regl-sketches/src/009/index.js
@@ -1,3 +1,17 @@
+const h = require('h');
+require('insert-css')(`
+.explanation {
+  position: absolute;
+  z-index: 100;
+  top: 5px;
+  left: 5px;
+  font-family: sans-serif;
+  font-style: italic;
+}
+.explanation h4 {
+  margin: 0;
+}
+`);
 const glsl = require('glslify');
 const mouse = require('mouse-event');
 const mouseWheel = require('mouse-wheel');
@@ -12,6 +26,12 @@ const regl = require('regl')({
 });
 
 function run (regl) {
+  const explanation = h('div.explanation', [
+    h('h4', 'Logistic Map'),
+    h('div', 'Drag and scroll to pan/zoom. Shift to zoom vertically')
+  ]);
+  document.body.appendChild(explanation);
+
   let accum = 0;
   const width = 512;
   const height = 512;
@@ -28,41 +48,75 @@ function run (regl) {
   }
 
   const params = {
-    xmin: 2.5,
+    xmin: 1,
     xmax: 4,
-    ymin: 0,
+    ymin: 0.0,
     ymax: 1,
   };
 
-  let previ;
-  mouseChange((btn, i) => {
+  let previ, prevj;
+  let shift = false;
+  mouseChange((btn, i, j, mods) => {
+    shift = mods.shift;
     if (btn === 1 && previ !== undefined) {
       let dx = (params.xmax - params.xmin) * (i - previ) / window.innerWidth;
+      let dy = -(params.ymax - params.ymin) * (j - prevj) / window.innerHeight;
       let xmin0 = params.xmin;
       let xmax0 = params.xmax;
+      let ymin0 = params.ymin;
+      let ymax0 = params.ymax;
       let newXmin = Math.max(1.0, xmin0 - dx);
       let newXmax = Math.min(4.0, xmax0 - dx);
-      if (newXmin > 0.0 && newXmax < 4.0 && (newXmin !== xmin0 || newXmax !== xmax0)) {
-        params.xmin = newXmin;
-        params.xmax = newXmax;
-        refresh();
+      let newYmin = Math.max(0.0, ymin0 - dy);
+      let newYmax = Math.min(1.0, ymax0 - dy);
+
+      params.xmin = Math.max(0, newXmin);
+      params.xmax = Math.min(4, newXmax);
+      params.ymin = Math.max(0, newYmin);
+      params.ymax = Math.min(1, newYmax);
+
+      if (newXmin !== xmin0 || newXmax !== xmax0 || newYmin !== ymin0 || newYmax !== ymax0) {
+        needsAccel = false;
+        needsQuick = true;
       }
     }
     previ = i;
+    prevj = j;
   });
 
   mouseWheel((dx, dy, dz, ev) => {
-    let x0 = params.xmin + (params.xmax - params.xmin) * mouse.x(ev) / window.innerWidth;
-    let xmin0 = params.xmin;
-    let xmax0 = params.xmax;
-    let xp = params.xmax - x0;
-    let xm = x0 - params.xmin;
-    let zoom = Math.exp(dy / 50.0);
-    xm *= zoom;
-    xp *= zoom;
-    params.xmin = Math.max(1.0, x0 - xm);
-    params.xmax = Math.min(4.0, x0 + xp);
-    if (params.xmin !== xmin0 || params.xmax !== xmax0) refresh();
+    if (shift) {
+      let y0 = params.ymin + (params.ymax - params.ymin) * (1 - mouse.y(ev) / window.innerHeight);
+      let ymin0 = params.ymin;
+      let ymax0 = params.ymax;
+      let yp = params.ymax - y0;
+      let ym = y0 - params.ymin;
+      let zoom = Math.exp(dy / 50.0);
+      ym *= zoom;
+      yp *= zoom;
+      params.ymin = Math.max(0, y0 - ym);
+      params.ymax = Math.min(1, y0 + yp);
+      if (params.ymin !== ymin0 || params.ymax !== ymax0) {
+        needsAccel = false;
+        needsQuick = true;
+      }
+
+    } else {
+      let x0 = params.xmin + (params.xmax - params.xmin) * mouse.x(ev) / window.innerWidth;
+      let xmin0 = params.xmin;
+      let xmax0 = params.xmax;
+      let xp = params.xmax - x0;
+      let xm = x0 - params.xmin;
+      let zoom = Math.exp(dy / 50.0);
+      xm *= zoom;
+      xp *= zoom;
+      params.xmin = Math.max(1.0, x0 - xm);
+      params.xmax = Math.min(4.0, x0 + xp);
+      if (params.xmin !== xmin0 || params.xmax !== xmax0) {
+        needsAccel = false;
+        needsQuick = true;
+      }
+    }
   });
 
   const samplerCoords = regl.buffer(new Array(width * height).fill(0).map((d, i) => [
@@ -70,16 +124,39 @@ function run (regl) {
     Math.floor(i / width) / Math.max(1, height - 1)
   ]));
 
-  const textures = new Array(2).fill(0).map(() =>
-    regl.texture({width: width, height: height, data: data, type: 'float32'})
-  );
+  const initial = regl.texture({width: width, height: height, data: data, type: 'float32'});
 
   const values = new Array(2).fill(0).map((d, i) =>
     regl.framebuffer({
-      color: textures[i],
+      color: regl.texture({width: width, height: height, data: data, type: 'float32'}),
       depth: false,
     })
   );
+
+  const initializeOp = regl({
+    vert: `
+      precision mediump float;
+      attribute vec2 xy;
+      varying vec2 uv;
+      void main () {
+        uv = 0.5 * (1.0 + xy);
+        gl_Position = vec4(xy, 0, 1);
+      }
+    `,
+    frag: `
+      precision mediump float;
+      varying vec2 uv;
+      uniform sampler2D src;
+      void main () {
+        gl_FragColor = texture2D(src, uv);
+      }
+    `,
+    framebuffer: regl.prop('dest'),
+    uniforms: {src: initial},
+    attributes: {xy: [[-4, -4], [0, 4], [4, -4]]},
+    depth: {enable: false},
+    count: 3
+  });
 
   const iterateOp = regl({
     vert: `
@@ -112,25 +189,17 @@ function run (regl) {
     count: 3
   });
 
-  const iterate = () => {
-    for (let i = needsAccel ? 100 : 1; i >= 0; i--) {
-      iterateOp({src: values[0], dest: values[1]})
-      swap(values);
-    }
-    accum++;
-    needsAccel = false;
-  };
-
   const drawPoints = regl({
     vert: `
       precision mediump float;
       attribute vec2 uv;
       uniform sampler2D data;
       uniform float select;
+      uniform vec4 viewport;
       void main () {
         vec4 p = texture2D(data, uv);
         vec2 y = p.xy * select + p.zw * (1.0 - select);
-        gl_Position = vec4(y.x, y.y * 2.0 - 1.0, 0, 1);
+        gl_Position = vec4(y.x, y.y * viewport.z + viewport.w, 0, 1);
         gl_PointSize = 1.0;
       }
     `,
@@ -145,7 +214,11 @@ function run (regl) {
       func: {srcRGB: 'src alpha', srcAlpha: 1, dstRGB: 1, dstAlpha: 1},
       equation: {rgb: 'add', alpha: 'add'}
     },
-    uniforms: {select: regl.prop('select')},
+    uniforms: {
+      data: regl.prop('src'),
+      select: regl.prop('select')
+    },
+    framebuffer: regl.prop('dest'),
     attributes: {uv: samplerCoords},
     primitive: 'points',
     count: width * height
@@ -153,12 +226,42 @@ function run (regl) {
 
   const swap = (arr) => {let tmp = arr[0]; arr[0] = arr[1]; arr[1] = tmp;}
 
-  const buffer = regl.framebuffer({
+  const buffers = new Array(2).fill(0).map(() => regl.framebuffer({
     width: regl._gl.canvas.width,
     height: regl._gl.canvas.height,
     colorType: 'float',
     depth: false,
-  })
+  }));
+
+  const decay = regl({
+    vert: `
+      precision mediump float;
+      attribute vec2 xy;
+      varying vec2 uv;
+      void main () {
+        uv = 0.5 * (1.0 + xy);
+        gl_Position = vec4(xy, 0, 1);
+      }
+    `,
+    frag: `
+      precision mediump float;
+      uniform sampler2D src;
+      varying vec2 uv;
+      uniform float decay;
+      void main () {
+        float col = texture2D(src, uv).x;
+        gl_FragColor = vec4(col * decay, 0, 0, 1);
+      }
+    `,
+    attributes: {xy: [[-4, -4], [0, 4], [4, -4]]},
+    uniforms: {
+      src: regl.prop('src'),
+      decay: () => decayConst,
+    },
+    framebuffer: regl.prop('dest'),
+    depth: {enable: false},
+    count: 3
+  });
 
   const drawToScreen = regl({
     vert: `
@@ -176,8 +279,8 @@ function run (regl) {
       uniform float scalar;
       varying vec2 uv;
       void main () {
-        vec3 value = texture2D(src, uv).xyz;
-        gl_FragColor = vec4(1.0 - value * scalar, 1);
+        float value = texture2D(src, uv).x;
+        gl_FragColor = vec4(vec3(1.0 - value * scalar), 1);
       }
     `,
     attributes: {xy: [[-4, -4], [0, 4], [4, -4]]},
@@ -187,43 +290,71 @@ function run (regl) {
   });
 
   const setUniforms = regl({uniforms: {
-    data: regl.prop('src'),
     scalar: ctx => {
       let ret = 0.1 / accum;
       ret *= 512 * 512 / width / height;
       ret /= 700 * 700 / ctx.viewportWidth / ctx.viewportHeight;
+      ret /= Math.sqrt(params.ymax - params.ymin);
       return ret;
     },
     viewport: () => [
       (params.xmin + params.xmax) * 0.5,
       0.5 * (params.xmax - params.xmin),
-      params.ymin, params.ymax
+      1.0 / (params.ymax - params.ymin) * 2,
+      -params.ymin / (params.ymax - params.ymin) * 2 - 1
     ]
   }});
 
-  let needsAccel = false;
+  let needsQuick = false;
+  let needsAccel = true;
+  let decayConst = 0.98;
+
+  const iterate = () => {
+    let iters = needsAccel ? 100 : (needsQuick ? 20 : 1);
+
+    if (needsQuick || needsAccel) refresh();
+
+    for (let i = iters; i >= 0; i--) {
+      iterateOp({src: values[0], dest: values[1]})
+      swap(values);
+    }
+
+    accum *= decayConst;
+    accum++;
+
+    if (needsQuick) {
+      needsQuick = false;
+      needsAccel = true;
+    } else if (needsAccel) {
+      needsAccel = false;
+      needsQuick = false;
+    }
+
+  };
+
   const refresh = () => {
-    textures[0]({width: width, height: height, data: data, type: 'float32'});
-    textures[1]({width: width, height: height, data: data, type: 'float32'});
-    buffer.use(() => regl.clear({color: [0, 0, 0, 1]}));
+    initializeOp({dest: values[0]});
+    buffers[0].use(() => regl.clear({color: [0, 0, 0, 1]}));
     accum = 0;
-    needsAccel = true;
   }
 
   refresh();
 
   regl.frame(({tick}) => {
-    if (tick % 5 === 1 || needsAccel) {
-      setUniforms({src: values[0]}, () => {
-        iterate()
-        buffer.use(() => drawPoints([{select: 0}, {select: 1}]));
-      });
+    setUniforms(() => {
+      iterate();
 
-      setUniforms({src: values[0]}, () => {
-        drawToScreen({src: buffer});
-      });
-    }
+      drawPoints([
+        {src: values[0], select: 0, dest: buffers[0]},
+        {src: values[0], select: 1, dest: buffers[0]}
+      ])
 
+      decay({src: buffers[0], dest: buffers[1]})
+
+      drawToScreen({src: buffers[0]});
+    });
+
+    swap(buffers);
   });
 
 }
